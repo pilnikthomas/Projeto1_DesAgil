73c3ab0152aa575ff761f51c52b747f9d4d4582a
Thomas Pilnik <thomaspo@al.insper.edu.br>
2018-02-28 13:20:48 -0300

    13:20

--- Raiz/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
+++ Raiz/src/main/java/br/pro/hashi/ensino/desagil/tequilada/Main.java
@@ -1,7 +1,37 @@
 package br.pro.hashi.ensino.desagil.tequilada;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Projeto 1");
+		
+		try {
+			
+			FileReader labirinto = new FileReader("labirinto.txt");
+			
+			BufferedReader ler_labirinto = new BufferedReader(labirinto);
+			
+			String linha = ler_labirinto.readLine();
+			
+			while(linha != null) {
+				caracter=linha.toCharArray();
+				
+				for(int i=0; i<caracter.length; i++){
+					if (caracter[i]==("#")) {
+						caracter[i]=("X");
+					
+					if (caracter[i]==(" ")) {
+						caracter[i]=(" ");
+					}
+				
+					}
+				}
+				System.out.println(caracter);
+			}
+		labirinto.close();
+		}
+		
 	}
 }
